c997cf478816abc851215ec781732099c0d0f930
pigeon9999 <platus1@naver.com>
2018-12-13 21:46:19 +0900

    총알 장전 수정
    수정

--- D3DX_PROJECT/cCharacter.cpp
+++ D3DX_PROJECT/cCharacter.cpp
@@ -190,6 +190,7 @@ void cCharacter::Update(cMyCharacter* m_MyCharacter, cSkinnedMesh* m_SkinnedMesh
 		if (TotalPeriod <= CurrentPeriod+0.15)
 		{
 			reloading = false;
+			g_pGameInfoManager->MaxBulletCount = 30;
 		}
 	}
 
@@ -202,7 +203,7 @@ void cCharacter::Update(cMyCharacter* m_MyCharacter, cSkinnedMesh* m_SkinnedMesh
 			beforeAnimNum = CurrentAnimNum;
 			m_MyCharacter->SetAnimationIndexBlend(CurrentAnimNum);
 			g_pGameInfoManager->GetMyInfo()->Status = CurrentAnimNum;
-			g_pGameInfoManager->MaxBulletCount = 30;
+			
 		}
 	}
 
